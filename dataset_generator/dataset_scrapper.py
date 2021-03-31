cf512e2a1bee9483ff119030020c55fe75fc459b
clarakreidy <clarakreidy@hotmail.com>
2021-03-31 23:15:15 +0300

    directly get data as csv from endpoint

---
+++ dataset_generator/dataset_scrapper.py
@@ -0,0 +1,15 @@
+import requests
+
+
+api = 'https://api.tiingo.com/tiingo/daily/eth/prices?startDate=2012-1-1&endDate=2021-3-1&format=csv'
+
+headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Token e8f19de4f1014da0c82def9df3edf347cc139584'
+}
+
+request = requests.get(api, headers=headers)
+
+csv_file = open('eth.csv', 'wb')
+csv_file.write(request.content)
+csv_file.close()
