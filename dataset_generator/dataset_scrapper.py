18ba3306cb19828757599add2112ac0b91a0bacb
clarakreidy <clarakreidy@hotmail.com>
2021-05-05 12:04:12 +0300

    logstash loads data to elasticsearch

--- dataset_generator/dataset_scrapper.py
+++ dataset_generator/dataset_scrapper.py
@@ -1,14 +1,41 @@
 import requests
+import io
+import pandas as pd
+import numpy as np
 
-
-api = 'https://api.tiingo.com/tiingo/daily/btcusd/prices?startDate=2006-1-1&endDate=2021-4-12&format=csv'
+api = 'https://api.tiingo.com/tiingo/daily/btcusd/prices?startDate=2006-1-1&endDate=2021-5-3&format=csv'
 
 headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Token e8f19de4f1014da0c82def9df3edf347cc139584'
 }
 
-request = requests.get(api, headers=headers)
-csv_file = open('eth.csv', 'wb')
-csv_file.write(request.content)
-csv_file.close()
+request = requests.get(api, headers=headers).content
+eth = pd.read_csv(io.StringIO(request.decode('utf-8')))
+
+# Bollinger Bands
+eth['movingAverage'] = eth.adjClose.rolling(20).mean()
+eth['standardDeviation'] = eth.adjClose.rolling(20).std()
+eth['BollingerBandsUpper'] = eth['movingAverage'] + (eth['standardDeviation'] * 2)
+eth['BollingerBandsLower'] = eth['movingAverage'] - (eth['standardDeviation'] * 2)
+
+
+# RSI
+N = 14
+
+
+def rma(x, n, y0):
+    a = (n-1) / n
+    ak = a**np.arange(len(x)-1, -1, -1)
+    return np.r_[np.full(n, np.nan), y0, np.cumsum(ak * x) / ak / n + y0 * a**np.arange(1, len(x)+1)]
+
+
+eth['change'] = eth['adjClose'].diff(1)
+eth['gain'] = eth.change.mask(eth.change < 0, 0.0)
+eth['loss'] = -eth.change.mask(eth.change > 0, -0.0)
+eth['AvgU'] = rma(eth.gain[N+1:].to_numpy(), N, np.nansum(eth.gain.to_numpy()[:N+1])/N)
+eth['AvgD'] = rma(eth.loss[N+1:].to_numpy(), N, np.nansum(eth.loss.to_numpy()[:N+1])/N)
+eth['rs'] = abs(eth['AvgU']/eth['AvgD'])
+eth['rsi'] = 100 - 100 / (1 + eth['rs'])
+
+eth.to_csv('ethereum.csv', index=False)
--- ui_setup/wizard/wizard.sh
+++ ui_setup/wizard/wizard.sh
@@ -3,25 +3,17 @@
 # Wait for Elasticsearch to start up before doing anything.
 until curl -s http://elasticsearch:9200/_cat/health -o /dev/null; do
     echo Waiting for Elasticsearch...
-    sleep 10
+    sleep 1
 done
 
 
 # Wait for Kibana to start up before doing anything.
-until curl -s http://kibana:5601api/task_manager/_health -o /dev/null; do
+until curl -s http://kibana:5601/api/task_manager/_health -o /dev/null; do
     echo Waiting for Kibana...
-    sleep 10
+    sleep 1
 done
 
 cd /tmp
-sleep 10
 
 #load dashboard
 curl -X POST http://kibana:5601/api/saved_objects/_import?createNewCopies=true -H "kbn-xsrf: true" --form file=@dashboard.ndjson
-
-#load data
-
-#until $(curl -X POST http://kibana:5601/api/saved_objects/_import?createNewCopies=true -H "kbn-xsrf: true" --form file=@dashboard.ndjson) != 'Kibana server is not ready yet'; do
-#    echo Waiting for Kibana...
-#    sleep 10
-#done
