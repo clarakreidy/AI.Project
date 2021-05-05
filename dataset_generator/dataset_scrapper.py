44c3d26fe1f6cd5b0bd091247d225ef953c4c944
clarakreidy <clarakreidy@hotmail.com>
2021-05-05 21:23:22 +0300

    testing errors - will be fixed soon

--- dataset_generator/dataset_scrapper.py
+++ dataset_generator/dataset_scrapper.py
@@ -2,8 +2,11 @@ import requests
 import io
 import pandas as pd
 import numpy as np
+from datetime import date, timedelta
 
-api = 'https://api.tiingo.com/tiingo/daily/btcusd/prices?startDate=2006-1-1&endDate=2021-5-3&format=csv'
+yesterday = date.today() - timedelta(days=1)
+
+api = 'https://api.tiingo.com/tiingo/daily/btcusd/prices?startDate=2005-1-1&endDate=' + yesterday.strftime('%Y-%m-%d') + '&format=csv'
 
 headers = {
     'Content-Type': 'application/json',
@@ -11,13 +14,13 @@ headers = {
 }
 
 request = requests.get(api, headers=headers).content
-eth = pd.read_csv(io.StringIO(request.decode('utf-8')))
+btc = pd.read_csv(io.StringIO(request.decode('utf-8')))
 
 # Bollinger Bands
-eth['movingAverage'] = eth.adjClose.rolling(20).mean()
-eth['standardDeviation'] = eth.adjClose.rolling(20).std()
-eth['BollingerBandsUpper'] = eth['movingAverage'] + (eth['standardDeviation'] * 2)
-eth['BollingerBandsLower'] = eth['movingAverage'] - (eth['standardDeviation'] * 2)
+btc['movingAverage'] = btc.adjClose.rolling(20).mean()
+btc['standardDeviation'] = btc.adjClose.rolling(20).std()
+btc['BollingerBandsUpper'] = btc['movingAverage'] + (btc['standardDeviation'] * 2)
+btc['BollingerBandsLower'] = btc['movingAverage'] - (btc['standardDeviation'] * 2)
 
 
 # RSI
@@ -30,12 +33,12 @@ def rma(x, n, y0):
     return np.r_[np.full(n, np.nan), y0, np.cumsum(ak * x) / ak / n + y0 * a**np.arange(1, len(x)+1)]
 
 
-eth['change'] = eth['adjClose'].diff(1)
-eth['gain'] = eth.change.mask(eth.change < 0, 0.0)
-eth['loss'] = -eth.change.mask(eth.change > 0, -0.0)
-eth['AvgU'] = rma(eth.gain[N+1:].to_numpy(), N, np.nansum(eth.gain.to_numpy()[:N+1])/N)
-eth['AvgD'] = rma(eth.loss[N+1:].to_numpy(), N, np.nansum(eth.loss.to_numpy()[:N+1])/N)
-eth['rs'] = abs(eth['AvgU']/eth['AvgD'])
-eth['rsi'] = 100 - 100 / (1 + eth['rs'])
+btc['change'] = btc['adjClose'].diff(1)
+btc['gain'] = btc.change.mask(btc.change < 0, 0.0)
+btc['loss'] = -btc.change.mask(btc.change > 0, -0.0)
+btc['AvgU'] = rma(btc.gain[N+1:].to_numpy(), N, np.nansum(btc.gain.to_numpy()[:N+1])/N)
+btc['AvgD'] = rma(btc.loss[N+1:].to_numpy(), N, np.nansum(btc.loss.to_numpy()[:N+1])/N)
+btc['rs'] = abs(btc['AvgU']/btc['AvgD'])
+btc['rsi'] = 100 - 100 / (1 + btc['rs'])
 
-eth.to_csv('ethereum.csv', index=False)
+btc.to_csv('bitcoin.csv', index=False)
